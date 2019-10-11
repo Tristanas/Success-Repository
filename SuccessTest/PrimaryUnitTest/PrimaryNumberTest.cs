1d8288e36083e7bb7f2d1280f982d2d355cfe143
Tristanas <43069390+Tristanas@users.noreply.github.com>
2019-10-11 12:43:56 +0300

    Added simple solution

---
+++ SuccessTest/PrimaryUnitTest/PrimaryNumberTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimaryUnitTest
+{
+    [TestClass]
+    public class PrimaryNumberTest
+    {
+        [TestMethod]
+        public void OneIsNotPrime()
+        {
+            PrimaryNumberChecker checker = new PrimaryNumberChecker();
+            bool isOnePrime = checker.isPrime(1);
+            
+        }
+    }
+}
---
+++ SuccessTest/SuccessTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SuccessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Error, insufficient arguments");
+                return;
+            }
+
+            int start = int.Parse(args[0]),
+                finish = int.Parse(args[1]);
+
+            if (start > finish)
+            {
+                Console.WriteLine("Error, interval start is higher than end.");
+                return;
+            }
+
+            PrimaryNumberChecker checker = new PrimaryNumberChecker();
+
+            Console.WriteLine($"Primary numbers in the range: [{1}, {2}]", start, finish);
+            for (int i = start; i <= finish; i++)
+            {
+                if (checker.isPrimary(i)) Console.WriteLine(i);    
+            }
+        }
+    }
+}
